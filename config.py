71dd1dc6c0536812236afc9554f8488a79ed79b4
Becelchak <88208993+Becelchak@users.noreply.github.com>
2026-04-04 11:10:11 +0500

    pre-commit and securiy-pipeline added
    Безопасность коммитов и дополнительная проверка во время пайплайна

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 # AWS Credentials для хранения отчётов
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", "AKIAIOSFODNN7EXAMPLE")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_S3_BUCKET = "taskflow-reports"
 AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
 
